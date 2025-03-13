80da2f039c2387d4927ebb819753f2410bfa9aeb
Katta Hima Vamsi <hvkatta@gmail.com>
2025-03-13 22:30:46 +0530

    Initial Commit

---
+++ src/main/java/com/himavamsi/copper_plus_plus/mixin/CopperMixin.java
@@ -0,0 +1,15 @@
+package com.himavamsi.copper_plus_plus.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class CopperMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
