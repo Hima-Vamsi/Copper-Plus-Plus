d97a47091cd653100de93b50f827661c50b99acd
Katta Hima Vamsi <hvkatta@gmail.com>
2025-03-24 18:52:31 +0530

    Add regular copper tools Features Added: 1. Copper Sword 2. Copper Shovel 3. Copper Pickaxe 4. Copper Axe 5. Copper Hoe 6. Copper++ Mod Item Group

--- src/main/java/com/himavamsi/copper_plus_plus/Copper.java
+++ src/main/java/com/himavamsi/copper_plus_plus/Copper.java
@@ -1,5 +1,6 @@
 package com.himavamsi.copper_plus_plus;
 
+import com.himavamsi.copper_plus_plus.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.apache.logging.log4j.LogManager;
@@ -7,10 +8,12 @@ import org.apache.logging.log4j.Logger;
 
 public class Copper implements ModInitializer {
 	public static final String MOD_ID = "copper_plus_plus";
+
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello from Copper++");
 	}
---
+++ src/main/java/com/himavamsi/copper_plus_plus/item/ModItemGroup.java
@@ -0,0 +1,13 @@
+package com.himavamsi.copper_plus_plus.item;
+
+import com.himavamsi.copper_plus_plus.Copper;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup COPPER = FabricItemGroupBuilder.build(new Identifier(Copper.MOD_ID, "copper"),
+            () -> new ItemStack(Items.COPPER_INGOT));
+}
